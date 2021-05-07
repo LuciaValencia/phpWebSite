76c7553edbf90d9c61a0d599a527616dd1e76d5d
LuciaValencia <82531083+LuciaValencia@users.noreply.github.com>
2021-05-07 11:34:10 +0200

    Delete PROVAVERA directory

--- PROVAVERA/mngm/connessioneDB.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $erroreDB="";
-	try {
-        $conn = new PDO("mysql:host=localhost;dbname=YouTravel", "root");
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    } catch(Exception $e){
-        $erroreDB = "La connessione non ha funzionato: ".$e->getMessage();
-    }
-?>
